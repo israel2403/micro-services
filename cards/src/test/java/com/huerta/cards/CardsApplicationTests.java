6544ade4f9dbe62321ba5127472a74db8c21e697
Israel Huerta <israelhf24@gmail.com>
2025-04-11 07:35:29 -0600

    Update CardsApplicationTests to specify server port in tests

--- cards/src/test/java/com/huerta/cards/CardsApplicationTests.java
+++ cards/src/test/java/com/huerta/cards/CardsApplicationTests.java
@@ -4,19 +4,21 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ConfigurableApplicationContext;
 
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
 public class CardsApplicationTests {
 
   @Test
   public void contextLoads() {
-    try (ConfigurableApplicationContext context = SpringApplication.run(CardsApplication.class)) {
+    try (ConfigurableApplicationContext context = SpringApplication.run(CardsApplication.class, "--server.port=0")) {
       assertNotNull(context);
     }
   }
 
   @Test
   public void main() {
-    CardsApplication.main(new String[] {});
+    CardsApplication.main(new String[] { "--server.port=0" });
   }
 }
